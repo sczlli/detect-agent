b8183a6d0352978b1b3c44203be5562ec9f63a06
Alessandro <alessandroscalzulli@gmail.com>
2025-07-27 20:40:22 +0200

    Feat: added loggin system initializer

--- logger.c
+++ logger.c
@@ -4,13 +4,12 @@
 #include <time.h>
 #include <string.h>
 
-//Static global variables to encapsulate the logger state
-static LogLevel current_log_level = LOG_LEVEL_INFO;
-static FILE* log_file = NULL;
+ //Static global variables to encapsulate the logger state
+ static LogLevel current_log_level = LOG_LEVEL_INFO;
+ static FILE* log_file = NULL;
 
-static const char* get_log_level_string(LogLevel level) {
-
-   switch (level) {
+ static const char* get_log_level_string(LogLevel level) {
+   switch (level) { 
        case LOG_LEVEL_DEBUG: return "DEBUG";
        case LOG_LEVEL_INFO:  return "INFO"; 
        case LOG_LEVEL_WARN:  return "WARN";
@@ -18,9 +17,30 @@ static const char* get_log_level_string(LogLevel level) {
        case LOG_LEVEL_FATAL: return "FATAL";
        default:              return "UNKNOWN";
    }
+ }
 
 
+ //initializes the logging system 
+ void logger_init(LogLevel level, const char* log_file_path) {
+    current_log_level = level;
+
+   if (log_file_path) {
+        // Open the file in append mode ("a") to preserve history
+        log_file = fopen(log_file_path, "a");
+        if (!log_file_path)  {
+            fprint(stderr, "ERROR: Could not open log file: %s\n", log_file_path);
+
+          }      
+     }
 }
+
+  
+ 
+
+   }   
+
+
+ 
 
 
 
